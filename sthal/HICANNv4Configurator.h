57297bbee34a0e9c4a9a8374f08a99ca990cd8b1
Jakob Kaiser <jakob.kaiser@kip.uni-heidelberg.de>
2019-11-08 13:42:12 +0100

    Add description to HICANNv4Configurator
    Add short text which describes the purpose of the HICANNv4Configurator.
    
    Change-Id: I2cafc238e0d2c4cef295bbbfa7c99bb2ac3a6f27

--- sthal/HICANNv4Configurator.h
+++ sthal/HICANNv4Configurator.h
@@ -6,7 +6,14 @@
 #include "sthal/ParallelHICANNv4Configurator.h"
 
 namespace sthal {
-
+/*
+ * The HICANNv4Configurator is used for sequential configuration, i.e configure one HICANN
+ * at the time, using the v4-specific floating gate programming scheme implemented in the
+ * Parallelv4Configurator. In addition, the Parallelv4Configurator adds configuration stages
+ * and user defined sleeps after each stage. When both, v4-FG-programming and configuration stages
+ * (with sleeps), should be used, the HICANNv4Configurator can be used instead of the
+ * HICANNConfigurator for sequential programming.
+ */
 class HICANNv4Configurator : public ParallelHICANNv4Configurator
 {
 };

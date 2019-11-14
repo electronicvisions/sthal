b84e6f681c0c9f2f9c5dadb8513fc89f463ac242
Eric Müller <mueller@kip.uni-heidelberg.de>
2019-11-14 11:45:41 +0100

    Hide STHAL_ARRAY_OPERATOR func body from py++
    Change-Id: I8ddb99d2f821316cd06ceedbc42339ceca168029

--- sthal/macros.h
+++ sthal/macros.h
@@ -1,3 +1,9 @@
+#ifndef PYPLUSPLUS
 #define STHAL_ARRAY_OPERATOR(type, coord, ...) \
 	type & operator[](const coord & ii) { __VA_ARGS__ } \
 	const type & operator[](const coord & ii) const { __VA_ARGS__ }
+#else
+#define STHAL_ARRAY_OPERATOR(type, coord, ...) \
+	type & operator[](const coord & ii); \
+	const type & operator[](const coord & ii) const;
+#endif
